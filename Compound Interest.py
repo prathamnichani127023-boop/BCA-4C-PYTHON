a2e900ba3e1df1f9ba6c7e20920b0542e71e7fd6
prathamnichani127023-boop <pratham.nichani127023@marwadiuniversity.ac.in>
2026-01-30 09:10:59 +0530

    Delete Compound Interest.py

--- Compound Interest.py
+++
@@ -1,9 +0,0 @@
-# Compound Interest
-
-p = float(input("Enter Amount: "))
-r = float(input("Enter Rate : "))
-t = float(input("Enter Time : "))
-
-ci = p * (1 + r / 100) ** t - p
-
-print("Compound Interset: ", ci)
